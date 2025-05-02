c906528271c9aab0f38dbb1467532d7a7309f62c
Your Name <donho4664@gmail.com>
2025-05-02 19:52:06 +0545

    menu

--- restaurant_menu/settings.py
+++ restaurant_menu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9-vh0ryo(x9gsxny!)342q+=l9_##9bh=-1v$c51kup%txudo3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '64.225.87.174']
 
 
 # Application definition
